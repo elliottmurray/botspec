b97dd80411ef940c06534034cf0127b3fe853a49
Elliott Murray <elliottmurray@gmail.com>
2018-09-09 09:37:52 +1000

    init commit

--- lib/botspec/lex/lex_service.rb
+++ lib/botspec/lex/lex_service.rb
@@ -9,8 +9,35 @@ module BotSpec
 
       def initialize(config)
         @config = config
-      end
+        lex_region = config[:region] || 'us-east-1:w'
+        #
+        #@credentials = AWS::CognitoIdentityCredentials.new({
+        #  IdentityPoolId: "#{lexRegion}:#{poolId}"
+        #});
 
+        @lex_client = Aws::Lex.new(
+           region: ENV["LEX_FAQ_AWS_REGION"],
+           access_key_id: ENV["LEX_FAQ_AWS_ACCESS_ID"],
+           secret_access_key: ENV["LEX_FAQ_AWS_SECRET_ACCESS_KEY"])
+
+      #  @lex = AWS::Lex.new({
+      #  })
+
+
+
+      end
+      def post_message message, user_id
+
+      resp = @lex_client.post_text({
+        bot_name: Lex::Bot::NAME,
+        bot_alias: Lex::Bot::VERSION,
+        user_id: user_id,
+        session_attributes: {},
+        input_text: message
+      })
+
+    sleep(1);
+      end
     end
   end
 end
--- spec/botspec/lex/lex_service_spec.rb
+++ spec/botspec/lex/lex_service_spec.rb
@@ -11,5 +11,10 @@ RSpec.describe 'aws lex service' do
 
   it 'posts message' do
     expect(@service).to_not  be_nil
+    expect(aws_lex).to receive(:post_text).with('pizza')
+
+    @service.post_interaction('pizza')
+
+
   end
 end
