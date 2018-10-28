93234c3d9767d5c9154c214e7d7c394fd76a1e76
Elliott Murray <elliottmurray@gmail.com>
2018-10-28 20:10:02 +1100

    Gemfile lock ignored

--- script/release.sh
+++ script/release.sh
@@ -4,7 +4,7 @@ set -e
 bundle exec bump ${1:-minor} --no-commit
 #bundle exec appraisal update
 bundle exec rake generate_changelog
-git add lib/botspec/version.rb 
+git add lib/botspec/version.rb CHANGELOG.md 
 git commit -m "chore(release): version $(ruby -r ./lib/botspec/version.rb -e "puts Botspec::VERSION")" && git push
 bundle exec rake release
 
