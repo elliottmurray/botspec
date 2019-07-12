441ba1aab2424c9a88a0f4a314ce9bfcb5da4506
David <dswensen@dius.com.au>
2019-07-11 22:21:37 -0700

    updating logger and removed need for init

--- lib/chatbot_helpers.rb
+++ lib/chatbot_helpers.rb
@@ -6,28 +6,32 @@ module ChatbotHelpers
   
   @client = nil
   @logger = nil
+  @response = nil
 
-  # this should be hidden nicely in a context method
-  def init_chatbot_client
-    @client = BotSpec::AWS::LexService.new({botname: 'WebUiOrderFlowers'})
-    @logger = Logger.new(STDOUT)
+  def chatbot_client
+    @client ||= BotSpec::AWS::LexService.new({botname: 'WebUiOrderFlowers'}) 
+  end
+
+  def the_logger
+    @logger = @logger ||= Logger.new(STDOUT)
     @logger.level = Logger::INFO
+    @logger
   end
   
   def user(utterance)
-    @logger.info "User says: " + utterance.inspect
-    @response = @client.post_message(utterance, 'user_id')
+    the_logger.info "User says: " + utterance.inspect
+    @response = chatbot_client.post_message(utterance, 'user_id')
   end
   
   def chatbot(expected_response)
-    @logger.info "Expected response: " + expected_response.inspect
-    @logger.info "Actual response:   " + @response.message.inspect
+    the_logger.info "Expected response: " + expected_response.inspect
+    the_logger.info "Actual response:   " + @response.message.inspect
     expect(@response.message).to match(expected_response)
   end
 
   def intent_fired(intent_details)
     expected_intent = intent_details.first
-    @logger.info "\n\n got this intent.data: " + @response.data.inspect
+    the_logger.info "\n\n got this intent.data: " + @response.data.inspect
 
     expected_intent.keys.each do |key|
       expect(@response.data[key]).to eq(expected_intent[key])
