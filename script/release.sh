fae194aef03f0758f9d781548656216178f48cc0
Elliott Murray <elliottmurray@gmail.com>
2019-08-07 09:29:34 +1000

    added ci skip to auto generated commits

--- script/release.sh
+++ script/release.sh
@@ -8,7 +8,7 @@ bundle exec bump ${MODE} --no-commit
 #bundle exec appraisal update
 bundle exec rake generate_changelog
 git add lib/botspec/version.rb CHANGELOG.md
-git commit -m "chore(release): version $(ruby -r ./lib/botspec/version.rb -e "puts Botspec::VERSION")" && git push
+git commit -m "chore(release) [ci skip]: version $(ruby -r ./lib/botspec/version.rb -e "puts Botspec::VERSION")" && git push
 bundle exec rake release
 
 # Dockerhub release
