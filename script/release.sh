243c7d91a9059c4a024d5f1c68f3d999b35fbb2f
Elliott Murray <elliottmurray@gmail.com>
2019-08-06 17:08:07 +1000

    wip on adding workflows to publish artifacts

--- script/release.sh
+++ script/release.sh
@@ -7,11 +7,11 @@ bundle exec bump ${MODE} --no-commit
 
 #bundle exec appraisal update
 bundle exec rake generate_changelog
-git add lib/botspec/version.rb CHANGELOG.md 
+git add lib/botspec/version.rb CHANGELOG.md
 git commit -m "chore(release): version $(ruby -r ./lib/botspec/version.rb -e "puts Botspec::VERSION")" && git push
 bundle exec rake release
 
 # Dockerhub release
-docker build -t botspec .
-docker tag botspec elliottmurray/botspec
-docker push elliottmurray/botspec
+#docker build -t botspec .
+#docker tag botspec elliottmurray/botspec
+#docker push elliottmurray/botspec
