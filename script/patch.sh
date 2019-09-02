c49546adf1bdf25b513b974d303d3af399f89dc3
Elliott Murray <elliottmurray@gmail.com>
2019-09-02 22:41:41 +1000

    Switch condition

--- script/patch.sh
+++ script/patch.sh
@@ -3,9 +3,11 @@ set -e
 
 tag=`git name-rev --tags --name-only $(git rev-parse HEAD)`
 
-if [ $tag == 'undefined' ] 
+if [ $tag != 'undefined' ] 
 then
   echo "Creating a commit for bumping purposes"
+ 
+ 
   RELEASE_SCRIPT="$( dirname "${BASH_SOURCE[0]}" )"/release.sh
   MODE="patch"
   source $RELEASE_SCRIPT
