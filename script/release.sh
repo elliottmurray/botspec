a195cf96cd2d2f8dae0841d1bb9dde9548c37033
Elliott Murray <elliottmurray@gmail.com>
2019-09-02 20:58:44 +1000

    Better filtering

--- script/release.sh
+++ script/release.sh
@@ -9,7 +9,7 @@ bundle exec bump ${MODE} --no-commit
 bundle exec rake generate_changelog
 git add lib/botspec/version.rb CHANGELOG.md
 git commit -m "chore(release): version $(ruby -r ./lib/botspec/version.rb -e "puts Botspec::VERSION")" && git push
-bundle exec rake release
+#bundle exec rake release
 
 # Dockerhub release
 #docker build -t botspec .
