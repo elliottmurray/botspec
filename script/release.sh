23e61855748eac5dcb88c432acd8fcee08819165
Elliott Murray <elliottmurray@gmail.com>
2019-09-02 22:56:02 +1000

    transient tag

--- script/release.sh
+++ script/release.sh
@@ -8,10 +8,10 @@ bundle exec bump ${MODE} --no-commit
 #bundle exec appraisal update
 bundle exec rake generate_changelog
 git add lib/botspec/version.rb CHANGELOG.md
-git commit -m "chore(release): version $(ruby -r ./lib/botspec/version.rb -e "puts Botspec::VERSION")" && git push
+git commit -m "chore(release): version $(ruby -r ./lib/botspec/version.rb -e "puts Botspec::VERSION")" 
+git tag circleci_current 
+git push
+
+
 #bundle exec rake release
 
-# Dockerhub release
-#docker build -t botspec .
-#docker tag botspec elliottmurray/botspec
-#docker push elliottmurray/botspec
