ab2d818873eeff8d5bdcc24185ff575e276c41b8
Elliott Murray <elliottmurray@gmail.com>
2019-09-03 09:05:58 +1000

    push tags

--- script/release.sh
+++ script/release.sh
@@ -8,9 +8,10 @@ bundle exec bump ${MODE} --no-commit
 #bundle exec appraisal update
 bundle exec rake generate_changelog
 git add lib/botspec/version.rb CHANGELOG.md
-git commit -m "chore(release): version $(ruby -r ./lib/botspec/version.rb -e "puts Botspec::VERSION")" 
-git tag circleci_current 
-git push
+git commit -m "chore(release): version $(ruby -r ./lib/botspec/version.rb -e "puts Botspec::VERSION")"
+git tag circleci_current
+#git push
+git push origin --tags
 
 
 #bundle exec rake release
